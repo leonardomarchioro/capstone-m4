496b1529f374e516865fc7b2dfa70e9629c46b7e
Rodrigo Tavares <78651787+regetag@users.noreply.github.com>
2022-05-17 19:22:04 -0300

    fix: removed dotenv and throw exception

--- src/services/login/userLogin.service.ts
+++ src/services/login/userLogin.service.ts
@@ -3,17 +3,16 @@ import { User } from "../../entities/User/user.entity";
 import { IUserLogin } from "../../interfaces/login";
 import { compare } from "bcryptjs";
 import jwt from "jsonwebtoken";
-import "dotenv/config";
 
 const userLoginService = async ({ email, password }: IUserLogin) => {
   const userRepository = AppDataSource.getRepository(User);
   const user = await userRepository.findOne({ where: { email } });
 
   if (!user) {
-    return "Wrong email/password";
+    throw "Wrong email/password";
   }
   if (!compare(password, user.password)) {
-    return "Wrong email/password";
+    throw  "Wrong email/password";
   }
 
   const token = jwt.sign({ userId: user.id }, String(process.env.SECRET_KEY), {
