1a75397faf9be1ff1a94b4156e9b5443a90d816a
Rodrigo Tavares <78651787+regetag@users.noreply.github.com>
2022-05-17 19:17:01 -0300

    fix: default response on userListAll.controller

--- src/controllers/users/userListAll.controller.ts
+++ src/controllers/users/userListAll.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import userListAllService from "../../services/users/userListAll.service";
 
 const userListAllController = async (request: Request, response: Response) => {
-  const userList = await userListAllService();
+  const userList = await userListAllService() || [];
 
   return response.status(200).json(userList);
 };
