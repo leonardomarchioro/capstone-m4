9c8195e93e95ed22f86cbe136316961b2649d157
Rodrigo Tavares <78651787+regetag@users.noreply.github.com>
2022-05-18 19:19:50 -0300

    fix: changed parameter name

--- src/services/users/roleUpdate.service.ts
+++ src/services/users/roleUpdate.service.ts
@@ -1,12 +1,12 @@
 import { AppDataSource } from "../../data-source";
 import { User } from "../../entities/User/user.entity";
 
-const roleUpdateService = async (userId: string, role: boolean) => {
+const roleUpdateService = async (userId: string, isSupplier: boolean) => {
   const userRepository = AppDataSource.getRepository(User);
 
   const user = await userRepository.findOne({ where: { id: userId } });
 
-  user.IsSupplier = role;
+  user.IsSupplier = isSupplier;
 
   await userRepository.save(user);
 
