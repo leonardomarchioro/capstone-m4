cf901e8809201348db04153e80fde555f457248c
Marcelo Alves <marcelo.asf92@gmail.com>
2022-05-17 22:29:49 -0300

    feat: inserting the AppError class and changing the app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
-import express from "express"
+import express, { Request, Response, NextFunction } from "express";
+import { AppError } from "./errors/appError";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.send("Hello world!!!!")
-})
+  return res.send("Hello world!!!!");
+});
 
-export { app }
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
+
+export { app };
