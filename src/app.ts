5807966ffa9f8f675a60740dc67de45348403a08
GabrielRD <grd342@outlook.com>
2022-05-19 16:23:59 -0300

    docs: initialConfig

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,19 @@ import userRoutes from "./routes/user/user.routes";
 import errorHandler from "./middlewares/errorHandler.middleware";
 import swaggerUi from "swagger-ui-express";
 
-import * as swaggerJson from "./";
-
 const app = express();
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup());
+import swaggerDocs from "./swagger.json";
 
 app.use(express.json());
 
 app.use("/user", userRoutes);
 
+app.use("/terms", (req, res) => res.send({ message: "A definir" }));
+
 app.use("/candidate", candidateRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get("/", (req, res) => {
   return res.send("Hello world!!!!");
