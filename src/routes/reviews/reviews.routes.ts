8c18f5a9ceeb8a55894f2361ea030869269180ff
Luiz Felipe OP <luiz.felipe221295@hotmail.com>
2022-05-20 14:54:29 -0300

    feat: listReview route

--- src/routes/reviews/reviews.routes.ts
+++ src/routes/reviews/reviews.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import createReviewController from "src/controllers/reviews/createReview.controller";
+import listReviewController from "src/controllers/reviews/listReviewController";
+import ensureAuth from "src/middlewares/ensureAuth.middleware";
 
 const reviewRouter = Router();
 
+reviewRouter.use(ensureAuth);
+
 reviewRouter.post("/:idJob", createReviewController);
+reviewRouter.get("/:idJob", listReviewController);
 
 export default reviewRouter;
