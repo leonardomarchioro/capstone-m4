e2a59d940b49ba1ed4ca099f1f48e5367a7653a7
Rodrigo Tavares <rodrigotvrs70@hotmail.com>
2022-05-24 09:22:21 -0300

    fix: broken code

--- src/middlewares/jobsMiddlewares/verifyOwnJobId.middleware.ts
+++ src/middlewares/jobsMiddlewares/verifyOwnJobId.middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import AppError from "../../errors/appError";
 import { prisma } from "@PrismaClient";
 
-const verifyOwnJob = async (
+export const verifyOwnJob = async (
   request: Request,
   response: Response,
   next: NextFunction
@@ -17,11 +17,11 @@ const verifyOwnJob = async (
     },
   });
 
-  if (job.userId !== userId) {
+  if (job.userId === userId) {
     throw new AppError(401, "Unauthorized!");
   }
 
-  if (job.supplierId === userId) {
+  if (job.status !== "available") {
     throw new AppError(401, "Unauthorized!");
   }
 
